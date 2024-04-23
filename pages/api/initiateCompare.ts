7ba798cc9ddfbd2d5d4e5f9c604e114cdd2ec1ab
Devansh Aryan <devansh.aryan@sprinklr.com>
2024-04-23 16:50:04 +0530

    using npx

--- pages/api/initiateCompare.ts
+++ pages/api/initiateCompare.ts
@@ -28,10 +28,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   process.nextTick(async () => {
     try {
       process.env.TARGET_URL = baseUrl;
-      await exec('yarn lost-pixel update');
+      await exec('npx lost-pixel update');
 
       process.env.TARGET_URL = compareUrl;
-      await exec('yarn lost-pixel');
+      await exec('npx lost-pixel');
 
       tasks.setTaskStatus(newTaskId, 'completed');
     } catch (error) {
